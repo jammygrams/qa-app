b92a1c23efdfc58f16a7a72ef12aa5bc226c25c3
jammygrams <jmahbubani@gmail.com>
2022-11-21 09:30:01 +0000

    app running

---
+++ streamlit_app/app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+from qaapi.src.utils import query_api
+
+DEFAULT_TITLE = "Ghostbusters II"
+DEFAULT_DOCUMENT = "DANA (setting the wheel brakes on the buggy) Thank you, Frank. I’ll get the hang of this eventually. She continues digging in her purse while Frank leans over the buggy and makes funny faces at the baby, OSCAR, a very cute nine-month old boy. FRANK (to the baby) Hiya, Oscar. What do you say, slugger? FRANK (to Dana) That’s a good-looking kid you got there, Ms. Barrett."
+DEFAULT_QUESTION = "How is Oscar related to Dana?"
+
+url = st.text_input("API URL", placeholder="e.g. https://5385-34-91-64-246.ngrok.io")
+title = st.text_input("Title", DEFAULT_TITLE)
+document = st.text_area("Body", DEFAULT_DOCUMENT)
+question = st.text_input("Question", DEFAULT_QUESTION)
+button = st.button("Recalculate")
+
+if url and (question or button):
+    api_url = f"{url}/api"
+    response = query_api(url=api_url, question=question, document=document, title=title)
+
+    st.write("Answer:\n", response)
