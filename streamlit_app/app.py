48d9d3401e6cbf68320c6dddff5c902b2d03bca0
jammygrams <jmahbubani@gmail.com>
2022-11-21 10:22:21 +0000

    try

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # TODO: need relative path for now to run on streamlit cloud 
-from ..qaapi.src.utils import query_api
+from qaapi.src.utils import query_api
 
 DEFAULT_TITLE = "Ghostbusters II"
 DEFAULT_DOCUMENT = "DANA (setting the wheel brakes on the buggy) Thank you, Frank. I’ll get the hang of this eventually. She continues digging in her purse while Frank leans over the buggy and makes funny faces at the baby, OSCAR, a very cute nine-month old boy. FRANK (to the baby) Hiya, Oscar. What do you say, slugger? FRANK (to Dana) That’s a good-looking kid you got there, Ms. Barrett."
