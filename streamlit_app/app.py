06e04b0f7d9f9f8cc5ce972c7c71b7cdd859c49d
jammygrams <jmahbubani@gmail.com>
2022-11-22 17:02:40 +0000

    gcp api; sidebar

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -5,8 +5,12 @@ from qaapi.src.utils import query_api
 DEFAULT_TITLE = "Ghostbusters II"
 DEFAULT_DOCUMENT = "DANA (setting the wheel brakes on the buggy) Thank you, Frank. I’ll get the hang of this eventually. She continues digging in her purse while Frank leans over the buggy and makes funny faces at the baby, OSCAR, a very cute nine-month old boy. FRANK (to the baby) Hiya, Oscar. What do you say, slugger? FRANK (to Dana) That’s a good-looking kid you got there, Ms. Barrett."
 DEFAULT_QUESTION = "How is Oscar related to Dana?"
+DEFAULT_URL = "https://qa-api-sm2m6pkoaq-od.a.run.app" # GCP qa-api URL
 
-url = st.text_input("API URL", placeholder="e.g. https://5385-34-91-64-246.ngrok.io")
+with st.sidebar:
+    url = st.text_input("API URL", DEFAULT_URL)
+
+
 title = st.text_input("Title", DEFAULT_TITLE)
 document = st.text_area("Body", DEFAULT_DOCUMENT)
 question = st.text_input("Question", DEFAULT_QUESTION)
